0e80512a92c963f63b4311a4570990c15d4a39cf
Alina.Malykhina <Malykhina.A.Vl@sberbank.ru>
2022-01-28 10:54:31 +0300

    - update

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { createIntents, createSystemScenario, createUserScenario, createMatchers } from '@salutejs/scenario';
+import { SmartAppBrainRecognizer } from '@salutejs/recognizer-smartapp-brain';
+import { SaluteMemoryStorage } from '@salutejs/storage-adapter-memory';
+
+import { saluteExpressMiddleware } from './middleware.ts';
+import * as dictionary from './mainPage.i18n';
+import model from 'intents.json';
+
+const port = process.env.PORT || 3000;
+const index = express();
+index().use(express.json());
+
+index.listen(port);
+
+const {intent } = createMatchers();
+
+
+const intents = createIntents(model);
+index.post('/index-connector',saluteExpressMiddleware({
+    intents,
+    recognizer: new SmartAppBrainRecognizer(),
+    systemScenario: createSystemScenario({
+        RUN_APP: ({req,res}) =>{
+            const keyset = req.i18n(dictionary);
+            res.setPronounceText(keyset('Привет'));
+        },
+        NO_MATCH: ({req,res}) =>{
+            const keyset = req.i18n(dictionary);
+            res.setPronounceText(keyset('404'));
+        },
+    }),
+    userScenario: createUserScenario({
+        calc: {
+            match: intent('/sum'),
+            handle: ({ req, res }) => {
+                const keyset = req.i18n(dictionary);
+                const { num1, num2 } = req.variables;
+
+                res.setPronounceText(
+                    keyset('{result}. Это было легко!', {
+                        result: Number(num1) + Number(num2),
+                    }),
+                );
+            },
+        },
+    }),
+    storage: new SaluteMemoryStorage()
+    }));
+
+
